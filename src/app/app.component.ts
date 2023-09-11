5d199c4c76e739a5f7c69b1fc8e57eb83b685607
mobinazarei1090 <105194951+mobinazarei1090@users.noreply.github.com>
2023-09-11 11:41:39 +0330

    add first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'p7';
+  isST: boolean =true;
+  imagess:number[]=[1,1,1]
+  user:{name:string,lastname:string,phone:string,email:string,age:number}={
+    name:"mobina",
+    lastname:"zarei",
+    phone:"0641964794",
+    email:"mobina@1090",
+    age:25
+  }
+  users:{name:string,lastname:string,phone:string,email:string}[]=[
+    {
+      name:"mobina",
+      lastname:"zarei",
+      phone:"0641964794",
+      email:"mobina@1090"
+    },
+    {
+      name:"ali",
+      lastname:"jenabi",
+      phone:"65416584",
+      email:"ali@1111"
+    }
+  ]
+  constructor() {
+    this.myAge(this.user.age)
+    console.table(this.users);
+  }
+
+  myAge(i:number){
+    if(i < 18){
+      this.isST=false;
+    }else{
+      this.isST=true;
+    }
+  }
+  myClick(){
+    console.log("mobina")
+  }
+  count:number=0
+  sumNumber(i:number){
+    this.count+=i;
+    console.log(this.count)
+  }
 }
